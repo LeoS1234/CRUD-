abb95426b2175f25fc320babb04932bf49e76f2c
LEO SANCHEZ <utm20040047@UTMA.EDU.MX>
2022-08-05 12:59:07 -0500

    CRUD FINALIZADO EMPRESAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActualizarEmpresaComponent } from './pages/crud/actualizar-empresa/actualizar-empresa.component';
+import { RegistrarEmpresaComponent } from './pages/crud/registrar-empresa/registrar-empresa.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActualizarEmpresaComponent,
+    RegistrarEmpresaComponent,
+    CrudComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
